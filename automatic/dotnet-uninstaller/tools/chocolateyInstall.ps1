4a68faa7a8c30a458a4b0a39c159ead1fb670757
Eliot Bytyqi <141611135+Eliot06@users.noreply.github.com>
2023-08-14 15:55:06 +0200

    Add .NET Uninstaller package (#42)
    Co-authored-by: Pascal Berger <pascal.berger@gmail.com>

---
+++ automatic/dotnet-uninstaller/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'; 
+
+$toolsPath   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$filePath = "$toolsPath\dotnet-core-uninstall-0.0.0.msi"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'msi' 
+  file         = Get-Item $filePath
+
+  softwareName  = 'Microsoft .NET Core SDK Uninstall Tool*' 
+
+  silentArgs    = "/quiet /norestart" 
+  validExitCodes= @(0, 3010, 1641)
+}
+Install-ChocolateyInstallPackage @packageArgs 
---
+++ automatic/dotnet-uninstaller/update.ps1
@@ -0,0 +1,40 @@
+import-module au
+import-module PowerShellForGitHub
+
+$repoOwner = 'dotnet'
+$repoName = 'cli-lab'
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge -NoSuffix -FileNameBase $Latest.FileName }
+
+function global:au_SearchReplace {
+    @{
+      ".\legal\VERIFICATION.txt" = @{
+        "(?i)(1\..+)\<.*\>"         = "`${1}<$($Latest.URL32)>"
+        "(?i)(checksum type:\s+).*" = "`${1} $($Latest.ChecksumType32)"
+        "(?i)(checksum:\s+).*"      = "`${1} $($Latest.Checksum32)"
+      }
+      'tools\chocolateyInstall.ps1' = @{
+        "(^[$]filePath\s*=\s*`"[$]toolsPath\\)(.*)`"" = "`$1$($Latest.FileName).$($Latest.FileType)`""
+      }
+    }
+  }
+
+function global:au_GetLatest {
+    $release = Get-GitHubRelease -OwnerName $repoOwner -RepositoryName $repoName -Latest
+    $version = $release.tag_name
+    if ($version.StartsWith('v')) {
+      $version = $version.Substring(1)
+    }
+    
+    $asset = Get-GitHubReleaseAsset -OwnerName $repoOwner -RepositoryName $repoName -ReleaseId $release.id | Where-Object name -match 'dotnet-core-uninstall-.*\.msi'
+    $url = $asset.browser_download_url
+  
+    @{
+      Version   = $version
+      URL32     = $url
+      Filename  = "dotnet-core-uninstall-${version}"
+      FileType  = 'msi'
+    }
+  }
+  
+  update -ChecksumFor none -NoCheckChocoVersion

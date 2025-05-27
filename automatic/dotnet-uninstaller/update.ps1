a450d2e76ade963113777b9735b9e9ec7fd73e10
Mark Strasser <50884015+mark-strasser-nnl@users.noreply.github.com>
2025-05-27 10:40:48 -0400

    Fix dotnet-uninstall asset URL (#78)

--- automatic/dotnet-uninstaller/update.ps1
+++ automatic/dotnet-uninstaller/update.ps1
@@ -26,9 +26,13 @@ function global:au_GetLatest {
       $version = $version.Substring(1)
     }
     
-    $asset = Get-GitHubReleaseAsset -OwnerName $repoOwner -RepositoryName $repoName -ReleaseId $release.id | Where-Object name -match 'dotnet-core-uninstall-.*\.msi'
+    $asset = Get-GitHubReleaseAsset -OwnerName $repoOwner -RepositoryName $repoName -ReleaseId $release.id | Where-Object name -match 'dotnet-core-uninstall.msi'
     $url = $asset.browser_download_url
   
+    if (!$url) {
+      throw "Asset not found"
+    }
+
     @{
       Version   = $version
       URL32     = $url
